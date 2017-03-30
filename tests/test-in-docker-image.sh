69e77c11029671cdf205a419e912b333e2acdf05
Johan Guldmyr <johan.guldmyr@csc.fi>
2017-03-30 08:03:51 +0300

    testing: only test --check

--- tests/test-in-docker-image.sh
+++ tests/test-in-docker-image.sh
@@ -133,7 +133,7 @@ function main(){
 #    test_install_requirements
     test_ansible_setup
     test_playbook_syntax
-    test_playbook
+#    test_playbook
     test_playbook_check
 #    extra_tests
 
